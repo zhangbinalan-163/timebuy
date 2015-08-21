84c005ab6bb66721225d9071e209101957533187
zhangbin2 <zhangbin2@tuniu.com>
2015-08-21 16:08:12 +0800

    增加了controller测试用例的例子

---
+++ timebuy-site/src/test/java/com/alan/app/timebuy/web/controller/RegisterControllerTest.java
@@ -0,0 +1,55 @@
+package com.alan.app.timebuy.web.controller;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * 注册API测试
+ * Created by zhangbinalan on 15/8/21.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextHierarchy({
+        @ContextConfiguration(name = "parent",locations = {"classpath*:springContext-core.xml","classpath*:springContext-dao.xml"}),
+        @ContextConfiguration(name = "child", locations = "classpath*:springContext-mvc.xml")
+})
+public class RegisterControllerTest {
+    private static Logger logger= LoggerFactory.getLogger(RegisterControllerTest.class);
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void sendUserRegSmsTest() throws Exception {
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/reg/sms").param("phone", "18069812065").header("x-timebuy-sid","test-sid");
+        MvcResult result = mockMvc.perform(request)
+                .andReturn();
+        String resultContent=result.getResponse().getContentAsString();
+        logger.info(resultContent);
+    }
+}
